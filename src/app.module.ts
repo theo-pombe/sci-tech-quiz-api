3120586402c397c8241a304b077eb1e72892849e
theo-pombe <theo.pombe@outlook.com>
2025-07-06 22:09:31 +0300

    create resource subjects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
-  imports: [],
+  imports: [SubjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
