8f0ef73d007fca32a3b056887e99261a056b38bd
theo-pombe <theo.pombe@outlook.com>
2025-07-06 22:54:22 +0300

    config mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubjectsModule } from './subjects/subjects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subject } from './subjects/entities/subject.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [SubjectsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Subject],
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // TODO: Turn off in production
+      }),
+    }),
+    SubjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
